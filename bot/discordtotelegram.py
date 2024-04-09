01d98901e0c08b250dcaa3a82471c6917a00486f
DasGortyCH <144722444+dasgortych@users.noreply.github.com>
2024-04-09 19:24:50 +0300

    Update discordtotelegram.py

--- bot/discordtotelegram.py
+++ bot/discordtotelegram.py
@@ -9,6 +9,6 @@ async def on_message (message):
   if message.channel.id not in cof['discord'] or message.author.bot:
     return
   
-  text = f'*{message.author.display_name}*' + '\n' + message.content
+  text = f'**{message.author.display_name}**' + '\n' + message.content
   for a in cof['telegram']:
     await bot.telegram.send_message(a, text)
